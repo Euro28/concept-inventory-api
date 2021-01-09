06d8d5b3240d328c45166cc388bec0b0c8e98929
Euro28 <eurol197628@gmail.com>
2021-01-09 22:37:17 +0000

    dependencies

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-import express from "express"
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+
+dotenv.config()
 
 const app = express();
-const port = 5000
+const port = 5000;
 
-app.get("/", (req,res) => res.send("hello world"));
+app.get("/", (req, res) => res.send("hello world"));
 
 app.listen(process.env.PORT || port, () => {
-    console.log("test running at port " + port)
-})
+  console.log("test running at port " + port);
+});
