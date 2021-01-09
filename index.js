c4132025bc91eef1f86caa19370746f75943de21
Euro28 <eurol197628@gmail.com>
2021-01-09 23:47:50 +0000

    router cors

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 import express from "express";
-import mongoose from "mongoose";
-import bodyParser from "body-parser";
-import cookieParser from "cookie-parser";
+// import mongoose from "mongoose";
+// import bodyParser from "body-parser";
+// import cookieParser from "cookie-parser";
 // import dotenv from "dotenv";
 
-import userRoute from "./src/routes/user.js";
+// import userRoute from "./src/routes/user.js";
 
 // dotenv.config();
 
 const app = express();
 const port = 5000;
 
-const dbOptions = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-};
+// const dbOptions = {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// };
 
-const connection = mongoose.connect(process.env.DB, dbOptions);
+// const connection = mongoose.connect(process.env.DB, dbOptions);
 
 app.get("/", (req, res) => res.send("hello world"));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(userRoute);
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(cookieParser());
+// app.use(userRoute);
 
 app.listen(port, () => {
   console.log("test running at port " + port);
