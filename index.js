fd8d9dfda835c3fd10dc4c9670dccb439846ee8e
Euro28 <eurol197628@gmail.com>
2021-01-10 18:41:04 +0000

    package.json

--- index.js
+++ index.js
@@ -5,10 +5,7 @@ import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 
 import userRoute from "./src/routes/user.js";
-<<<<<<< HEAD
 import cors from "./src/cors.js"
-=======
->>>>>>> 56d1f058767a077d999a2961350f42edc3679fec
 
 dotenv.config();
 
@@ -21,7 +18,6 @@ const dbOptions = {
 };
 
 const connection = mongoose.connect(process.env.DB, dbOptions);
-<<<<<<< HEAD
 
 //middleware
 app.use(cors);
@@ -30,37 +26,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 //routes
-=======
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "*");
-
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Pass to next layer of middleware
-  next();
-});
-
-app.get("/", (req, res) => res.send("hello world"));
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
->>>>>>> 56d1f058767a077d999a2961350f42edc3679fec
 app.use(userRoute);
 
 app.listen(process.env.PORT || port, () => {
