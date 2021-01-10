d06ee380bdad88597618d50c115c7f0ce6dbb315
Euro28 <eurol197628@gmail.com>
2021-01-10 15:54:47 +0000

    results get

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const dbOptions = {
 const connection = mongoose.connect(process.env.DB, dbOptions);
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
   // Request methods you wish to allow
   res.setHeader(
