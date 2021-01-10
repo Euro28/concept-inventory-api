4e48497bf6d30b40b08913b4d5362e0553a9afd6
Euro28 <eurol197628@gmail.com>
2021-01-10 17:34:49 +0000

    results

--- src/routes/user.js
+++ src/routes/user.js
@@ -36,9 +36,7 @@ router.post("/api/login", async (req, res) => {
   try {
     if (req.query.name) req.body.name = req.query.name;
 
-    console.log("req.body.name", req.body.name);
     const user = await User.findOne({ name: req.body.name });
-    console.log("user", user);
     if (!user) {
       res.status(204).send();
     } else {
@@ -64,5 +62,20 @@ router.get("/api/results", getUser, async (req, res) => {
     res.status(401).send(err);
   }
 });
+
+router.post("/api/results", async (req, res) => {
+  try {
+    const { name } = req.query
+    const { results } = req.body
+    const user = await User.findOne({ name })
+    user.results = results;
+
+    await user.save()
+
+    res.status(200).send(user)
+  } catch (err) {
+    res.status(401).send(err)
+  }
+})
 
 export default router;
