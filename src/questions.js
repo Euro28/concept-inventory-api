1467550addb737adc5e564a453f77a90b9679e27
Euro28 <eurol197628@gmail.com>
2021-01-15 19:49:47 +0000

    no images

--- src/questions.js
+++ src/questions.js
@@ -6,36 +6,14 @@ const json = {
    "elements": [
     {
      "type": "radiogroup",
-     "name": "Set question",
-     "title": "Let S be a well defined set (a set with no ambiguity on whether an object belongs to the set), for example C = {a,b,c} is well-defined as it is clear what is and is not in the set. Let {} be the empty set with no elements, which of the following statements are always true. (PICK ONE)",
-     "isRequired": true,
-     "choices": [
-      {
-       "value": "set-1",
-       "text": "{}  ∈  S"
-      },
-      {
-       "value": "correct",
-       "text": "{} ⊆ S"
-      },
-      {
-       "value": "set-2",
-       "text": "{} ⊂ S"
-      },
-      {
-       "value": "set-3",
-       "text": "{} = {{}}"
-      }
-     ]
-    },
-    {
-     "type": "radiogroup",
      "name": "question9",
      "title": "Suppose each card has a number on one side and a letter on the other. Which cards are relevant to turn over if you want to know whether the following statement is false? If a card has a vowel on one side, then it has an even number on the other side.",
+     "valueName": "implication",
+     "correctAnswer": "correct",
      "isRequired": true,
      "choices": [
       {
-       "value": "implication-1",
+       "value": "a",
        "text": "The A card"
       },
       {
@@ -43,67 +21,57 @@ const json = {
        "text": "The A card and the 7 card"
       },
       {
-       "value": "implication-2",
+       "value": "b",
        "text": "The A card and the 4 card"
       },
       {
-       "value": "implication-3",
+       "value": "c",
        "text": "The A, 4, and 7 card"
       },
       {
-       "value": "implication-4",
+       "value": "d",
        "text": "All the cards"
       }
      ]
     },
     {
-     "type": "image",
-     "name": "question10",
-     "startWithNewLine": false,
-     "imageLink": "https://i.ibb.co/5GLyqdH/cards.png",
-     "imageHeight": 250,
-     "imageWidth": 500
-    },
-    {
      "type": "radiogroup",
      "name": "question12",
-     "title": "If the teacher marks all the exam papers, they will get paid 50 pounds, the teacher did not get paid 50 pounds, which of the following conclusions are valid? (select one)",
+     "title": "If the teacher marks all the exam papers, they will get paid 50 pounds, the teacher did not get paid 50 pounds, which of the following conclusions are valid?",
+     "valueName": "implication",
+     "correctAnswer": "correct",
      "isRequired": true,
      "choices": [
       {
-       "value": "implication-1",
+       "value": "a",
        "text": "The teacher marked the papers"
       },
       {
-       "value": "implication-2",
+       "value": "b",
        "text": "The teacher did not mark the papers"
       },
       {
-       "value": "implication-3",
+       "value": "c",
        "text": "If the teacher got paid 50 pounds then they marked all the papers"
       },
       {
-       "value": "implication-4",
+       "value": "d",
        "text": "The teacher did not get the papers"
       },
       {
        "value": "correct",
        "text": "None of the above"
       }
-     ],
-     "colCount": 0
+     ]
     },
     {
-     "type": "html",
-     "name": "question1",
-     "html": "<img src=\"https://latex.codecogs.com/gif.latex?\\inline&space;\\dpi{200}&space;\\huge&space;\\neg&space;p&space;\\rightarrow&space;q&space;\\newline&space;\\indent&space;p\" title=\"\\huge \\neg p \\rightarrow q \\newline \\indent p\" />"
-    },
-    {
-     "type": "checkbox",
+     "type": "radiogroup",
      "name": "question13",
      "minWidth": "",
      "maxWidth": "600",
-     "title": "Tick all check boxes which can be deduced assuming the two statements above are true",
+     "title": "Given that ~p -> q, p are both true, which statement can be deduced",
+     "valueName": "denial of antecedent",
+     "correctAnswer": "correct",
      "isRequired": true,
      "choices": [
       {
@@ -111,46 +79,27 @@ const json = {
        "text": "p"
       },
       {
-       "value": "denial of antecedent-1",
+       "value": "a",
        "text": "~p"
       },
       {
-       "value": "denial of antecedent-1",
+       "value": "b",
        "text": "q"
       },
       {
-       "value": "denial of antecedent-1",
+       "value": "c",
        "text": "~q"
-      },
-      {
-       "value": "denial of antecedent-1",
-       "text": "p and ~p"
-      },
-      {
-       "value": "denial of antecedent-1",
-       "text": "p and q"
-      },
-      {
-       "value": "denial of antecedent-1",
-       "text": "~p and q"
-      },
-      {
-       "value": "denial of antecedent-1",
-       "text": "None of the above"
       }
      ]
     },
     {
-     "type": "html",
-     "name": "question14",
-     "html": "<img src=\"https://latex.codecogs.com/gif.latex?\\inline&space;\\dpi{200}&space;\\huge&space;\\neg&space;p&space;\\rightarrow&space;\\neg&space;q&space;\\newline&space;\\indent&space;\\neg&space;q\" title=\"\\huge \\neg p \\rightarrow \\neg q \\newline \\indent \\neg q\" />"
-    },
-    {
-     "type": "checkbox",
+     "type": "radiogroup",
      "name": "question15",
      "minWidth": "",
      "maxWidth": "600",
-     "title": "Tick all check boxes which can be deduced assuming the two statements above are true",
+     "title": "Given that ~p => ~q, ~q are both true, which statement can be deduced",
+     "valueName": "affirmation of consequent",
+     "correctAnswer": "correct",
      "isRequired": true,
      "choices": [
       {
@@ -159,7 +108,7 @@ const json = {
       },
       {
        "value": "b",
-       "text": "~p"
+       "text": "¬p"
       },
       {
        "value": "c",
@@ -167,33 +116,25 @@ const json = {
       },
       {
        "value": "correct",
-       "text": "~q"
+       "text": "¬q"
       },
       {
        "value": "d",
        "text": "None of the above"
-      },
-      {
-       "value": "item1",
-       "text": "p and ~p"
-      },
-      {
-       "value": "item2",
-       "text": "c"
       }
      ]
-    },
-    {
-     "type": "html",
-     "name": "question6",
-     "html": "<img src=\"https://latex.codecogs.com/gif.latex?\\inline&space;\\dpi{200}&space;\\huge&space;\\neg&space;p&space;\\lor&space;q&space;\\newline&space;\\indent&space;\\neg&space;q\" title=\"\\huge \\neg p \\lor q \\newline \\indent \\neg q\" />"
     },
     {
      "type": "checkbox",
      "name": "Disjunctive denial",
      "minWidth": "",
      "maxWidth": "600",
-     "title": "Tick all check boxes which can be deduced assuming the two statements above are true",
+     "title": "Given that ¬p v q, ¬q are both true, which statement can be deduced",
+     "valueName": "disjunctive denial",
+     "correctAnswer": [
+      "b",
+      "e"
+     ],
      "isRequired": true,
      "choices": [
       {
@@ -202,7 +143,7 @@ const json = {
       },
       {
        "value": "b",
-       "text": "~p"
+       "text": "¬p"
       },
       {
        "value": "c",
@@ -210,25 +151,20 @@ const json = {
       },
       {
        "value": "e",
-       "text": "~q"
+       "text": "¬q"
       },
       {
        "value": "d",
        "text": "None of the above"
       }
      ]
-    },
-    {
-     "type": "html",
-     "name": "question8",
-     "html": "<img src=\"https://latex.codecogs.com/gif.latex?\\inline&space;\\dpi{200}&space;\\huge&space;p&space;\\lor&space;\\neg&space;q&space;\\newline&space;\\indent&space;\\neg&space;q\" title=\"\\huge p \\lor \\neg q \\newline \\indent \\neg q\" />"
     },
     {
      "type": "checkbox",
      "name": "disjunctive affirmation",
      "minWidth": "",
      "maxWidth": "600",
-     "title": "Tick all check boxes which can be deduced assuming the two statements above are true",
+     "title": "Given that p v ~q, ~q are both true, which statement can be deduced.",
      "isRequired": true,
      "choices": [
       {
@@ -252,18 +188,13 @@ const json = {
        "text": "None of the above"
       }
      ]
-    },
-    {
-     "type": "html",
-     "name": "question7",
-     "html": "<img src=\"https://latex.codecogs.com/gif.latex?\\inline&space;\\dpi{200}&space;\\huge&space;p&space;\\rightarrow&space;q&space;\\newline&space;\\indent&space;p\" title=\"\\huge p \\rightarrow q \\newline \\indent p\" />"
     },
     {
      "type": "checkbox",
      "name": "modus ponens",
      "minWidth": "",
      "maxWidth": "600",
-     "title": "Tick all check boxes which can be deduced assuming the two statements above are true",
+     "title": "Given that p -> q, p are both true, which statement can be deduced",
      "isRequired": true,
      "choices": [
       {
@@ -287,18 +218,13 @@ const json = {
        "text": "None of the above"
       }
      ]
-    },
-    {
-     "type": "html",
-     "name": "question11",
-     "html": "<img src=\"https://latex.codecogs.com/gif.latex?\\inline&space;\\dpi{200}&space;\\huge&space;p&space;\\rightarrow&space;q&space;\\newline&space;\\indent&space;\\neg&space;q\" title=\"\\huge p \\rightarrow q \\newline \\indent \\neg q\" />"
     },
     {
      "type": "checkbox",
      "name": "modus tollens",
      "minWidth": "",
      "maxWidth": "600",
-     "title": "Tick all check boxes which can be deduced assuming the two statements above are true",
+     "title": "Given that p -> q, ~q are both true which statement can be deduced",
      "isRequired": true,
      "choices": [
       {
@@ -324,14 +250,9 @@ const json = {
      ]
     },
     {
-     "type": "html",
-     "name": "question2",
-     "html": "<img src=\"https://latex.codecogs.com/gif.latex?\\inline&space;\\dpi{200}&space;\\huge&space;\\neg&space;p&space;\\leftrightarrow&space;q\" title=\"\\huge \\neg p \\leftrightarrow q\" />"
-    },
-    {
      "type": "checkbox",
      "name": "biconditional question",
-     "title": "Check all boxes which results in the statement above holding in the given circumstance",
+     "title": "Tick all statements for which ~p <-> q evaluates to true, (<-> is if and only if)",
      "description": "correct ans = b,c",
      "isRequired": true,
      "choices": [
@@ -354,16 +275,9 @@ const json = {
      ]
     },
     {
-     "type": "image",
-     "name": "question3",
-     "imageLink": "https://i.ibb.co/JH3jP0L/comp.png",
-     "imageHeight": 100,
-     "imageWidth": 900
-    },
-    {
      "type": "radiogroup",
      "name": "negation question",
-     "title": "Consider the above statement The negation of this statement is",
+     "title": "What is the negation of the statement \"Either -2 <= x <= -1 OR 1 <= x <= 2\"",
      "isRequired": true,
      "choices": [
       {
